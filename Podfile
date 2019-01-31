14ce1002d2fae3ed261976f0ccde167f4c550fd4
Hugo <hugo444312@gmail.com>
2019-01-31 16:54:31 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ec-calendar' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ec-calendar
+  pod 'SnapKit', '~> 4.0.0'
+  pod 'ReactiveCocoa', '~> 8.0'
+  
+end
---
+++ ec-calendar/Launch/LaunchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ec-calendar
+//
+//  Created by Hugo on 31/1/2019.
+//  Copyright © 2019 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
