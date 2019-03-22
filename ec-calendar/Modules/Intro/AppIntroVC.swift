3cccee4fe39233eb5adca04aead5334e66656440
rzc <reneechan122237@gmail.com>
2019-03-22 22:01:49 +0800

    up

--- ec-calendar/Modules/Intro/AppIntroVC.swift
+++ ec-calendar/Modules/Intro/AppIntroVC.swift
@@ -14,59 +14,81 @@ class AppIntroVC: UIViewController {
     
     let introVC: OnboardingViewController = {
         let vc = OnboardingViewController()
+        vc.backgroundImage = UIImage(named: "finalBackground")
         return vc
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         initIntroView()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated);
-        self.navigationController?.setNavigationBarHidden(true, animated: animated);
-    }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated);
-        self.navigationController?.setNavigationBarHidden(false, animated: animated);
-    }
-
     func initIntroView() -> Void
     {
-        let firstPage = OnboardingContentViewController(title: "First page", body: "Welcome ", image: nil, buttonText: Helper.Localized(key: "common_nextpage"), action: ({
+        let firstPage = OnboardingContentViewController(title: "EC Diary", body: nil, image: UIImage(named: "launch"), buttonText: "next", action: ({
             //TODO action
             self.introVC.moveNextPage()
             
+            
         }))
         
-        let secondPage = OnboardingContentViewController(title: "second page", body: "Welcome ", image: nil, buttonText: Helper.Localized(key: "common_nextpage"), action: ({
+        let secondPage = OnboardingContentViewController(title: "EC Calendar", body: "A simple calendar ", image: UIImage(named: "launch"), buttonText: "next", action: ({
             //TODO action
             self.introVC.moveNextPage()
         }))
         
-        let thirdPage = OnboardingContentViewController(title: "third page", body: "Welcome ", image: nil, buttonText: Helper.Localized(key: "common_nextpage"), action: ({
+        
+        
+        let thirdPage = OnboardingContentViewController(title: "EC Calendar", body: "Easy to remember what we did in that day", image: UIImage(named: "acloud"), buttonText: "next", action: ({
             //TODO action
             self.introVC.skipButton.sendActions(for: UIControl.Event.touchUpInside)
             
         }))
+        
+        let forthPage = OnboardingContentViewController(title: "EC Calendar", body: "Reuse the calendar", image: UIImage(named: "tree"), buttonText: "next", action: ({
+            //TODO action
+            self.introVC.skipButton.sendActions(for: UIControl.Event.touchUpInside)
+            
+        }))
+        
+        let fifthPage = OnboardingContentViewController(title: "EC Diary", body: nil, image: UIImage(named: "note"), buttonText: "next", action: ({
+            //TODO action
+            self.introVC.skipButton.sendActions(for: UIControl.Event.touchUpInside)
+            
+        }))
+        
+        
+        
+        firstPage.topPadding = 70
+        firstPage.underIconPadding = 20
+        firstPage.underTitlePadding = 85
+        firstPage.bottomPadding = 70
+        firstPage.iconWidth = 250
+        firstPage.iconHeight = 128
+
+        
+        
+        
+        
         //add pages
-        self.introVC.viewControllers = [firstPage, secondPage, thirdPage]
+        self.introVC.viewControllers = [firstPage, secondPage, thirdPage, forthPage, fifthPage]
         //add skip button
         self.introVC.skipButton = addSkipButton(parent: self.introVC.view)
         //add intro vc
         self.addChild(self.introVC)
         self.view.addSubview(self.introVC.view)
         
-
+        
+        
         
     }
     
     func addSkipButton(parent:UIView) -> UIButton {
         let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
-        btn.setTitle(Helper.Localized(key: "common_skip"), for: UIControl.State.normal)
+        btn.setTitle("Skip", for: UIControl.State.normal)
         
         parent.addSubview(btn)
         
@@ -76,13 +98,13 @@ class AppIntroVC: UIViewController {
         }
         
         btn.reactive.controlEvents(UIControl.Event.touchUpInside).observe { _ in
-//            self.navigationController?.pushViewController(TestViewController(), animated: true)
-            self.navigationController?.setViewControllers([HomeVC()], animated: true);
+            self.navigationController?.pushViewController(TestViewController(), animated: true)
         }
-
-
+        
+        
         
         return btn
     }
-
+    
+    
 }
